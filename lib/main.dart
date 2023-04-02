f5149246cca6d2da5f9d7de4887b0e66bf4869fa
‘terupro’ <terupro911@gmail.com>
2023-04-02 15:09:17 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:allen/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:allen/pallete.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Allen',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
